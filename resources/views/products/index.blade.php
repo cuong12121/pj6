ce2056cc0ce8cd9d258ed64642f75f6d44307ef1
cuong <cuongdao2692@gmail.com>
2024-04-04 21:28:14 +0700

    update code

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -22,6 +22,13 @@
 
     </button>
 
+     <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#priceShowProduct">
+    
+        Chính sách giá cho sản phẩm
+
+    </button>
+
     <section class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
@@ -102,6 +109,46 @@
             </div>
         </div>
     </div>
+
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="priceShowProduct" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Danh sách chính sách giá</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div>
+                        <ul>
+
+                            <?php 
+
+                            $data_price_show = DB::table('show_price_address')->get();
+
+
+                            ?>
+                            @if(!empty($data_price_show))
+                            @foreach($data_price_show as $val)
+                            <li>{{ $val->name }} : {{ $val->price }}</li>
+                            @endforeach
+                            @endif
+                        </ul>
+                        
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+
 
 
     <?php 
