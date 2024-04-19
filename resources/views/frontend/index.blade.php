98a19276990581021a6322ea570b3e770e8c6126
cuong12121 <cuongdao12121@gmail.com>
2024-04-19 13:59:05 +0700

    update code

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -149,8 +149,8 @@
                                 <span class="title-end">Kết thúc sau</span>
                                 <span class="countdown-timer">
                                     <label id="hours">{{ $hour }}</label>
-                                    <label id="minutes">{{ $minutes }}</label>
-                                    <label id="seconds">{{ $seconds }}</label>
+                                    <label id="minutes">{{  intval($minutes)<10?'0'.$minutes:$minutes }}</label>
+                                    <label id="seconds">{{  intval($seconds)<10?'0'.$seconds:$seconds }}</label>
                                 </span>
                             </div>
                         </div>
