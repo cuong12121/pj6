cb8aad200ed76ca023adccefb55559e5d159550e
cuong12121 <cuongdao12121@gmail.com>
2024-04-19 15:10:56 +0700

    update

--- resources/views/css/fileCss.blade.php
+++ resources/views/css/fileCss.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-
+<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
 <link rel="stylesheet" type="text/css" href="{{ asset('lined-textarea/jquery-linedtextarea.css') }}">
 <script src="{{ asset('lined-textarea/jquery-linedtextarea.js') }}"></script>
 
