8dc40c59c8462841ca2faa82c08d7dfbc1db4173
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-04-22 20:30:52 +0700

    Update index.blade.php

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -405,9 +405,9 @@
                                     @foreach($data as $key =>$datas)
                                     
 
-                                    <a class="product-render rd row">
-                                        <div class="col-md-3 col-xs-6">
-                                            <div class="">
+                                    <a class="product-render rd">
+                                        <div class="row">
+                                            <div class="col-md-6 col-xs-3">
                                                 <div class="product-header" href="{{ route('details', $datas->Link) }}">
                                                     <div class="top-right">
                                                         <div class="product-feature-badge-item installment"><span>Trả góp 0%</span></div>
