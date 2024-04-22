f316dba2442d56ddd5253cbd3c317d9f90c51217
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-04-22 20:40:38 +0700

    Update index.blade.php

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -404,37 +404,36 @@
 
                                     @foreach($data as $key =>$datas)
 
-                                    <a class="product-render rd">
-                                       
-                                            <div class="product">
-                                                <div class="product-header" href="{{ route('details', $datas->Link) }}">
-                                                    <div class="top-right">
-                                                        <div class="product-feature-badge-item installment"><span>Trả góp 0%</span></div>
-                                                    </div>
-                                                    <div class="product-image">
-                                                        <a href="{{ route('details', $datas->Link) }}">
-                                                            <img
-                                                                class="ls-is-cached lazyloaded"
-                                                                
-                                                                src="{{ asset($datas->Image) }}"
-                                                                
-                                                            />
-                                                        </a>
-                                                    </div>
+                                   
+                                        <div class="product">
+                                            <div class="product-header" href="{{ route('details', $datas->Link) }}">
+                                                <div class="top-right">
+                                                    <div class="product-feature-badge-item installment"><span>Trả góp 0%</span></div>
+                                                </div>
+                                                <div class="product-image">
+                                                    <a href="{{ route('details', $datas->Link) }}">
+                                                        <img
+                                                            class="ls-is-cached lazyloaded"
+                                                            
+                                                            src="{{ asset($datas->Image) }}"
+                                                            
+                                                        />
+                                                    </a>
+                                                </div>
+                                                
+                                            </div>
+                                            <div class="product-body">
+                                                <div class="product-feature-badge"></div>
+                                                <div class="product-title"><a href="{{ route('details', $datas->Link) }}">{{ $datas->Name }}</a></div>
+                                                <div class="product-price">
+                                                    <p class="final-price">{{ @str_replace(',' ,'.', number_format($datas->Price)) }}đ   </p>
                                                     
                                                 </div>
-                                                <div class="product-body">
-                                                    <div class="product-feature-badge"></div>
-                                                    <div class="product-title"><a href="{{ route('details', $datas->Link) }}">{{ $datas->Name }}</a></div>
-                                                    <div class="product-price">
-                                                        <p class="final-price">{{ @str_replace(',' ,'.', number_format($datas->Price)) }}đ   </p>
-                                                        
-                                                    </div>
-                                                </div>
-                                                <div class="product-footer"></div>
                                             </div>
+                                            <div class="product-footer"></div>
+                                        </div>
                                       
-                                    </a>
+                                  
 
                                     @endforeach
 
