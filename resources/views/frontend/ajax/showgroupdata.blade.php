e19754a0171dcb7f3e21d318c014664adc9fbdf1
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-04-23 00:18:18 +0700

    Update showgroupdata.blade.php

--- resources/views/frontend/ajax/showgroupdata.blade.php
+++ resources/views/frontend/ajax/showgroupdata.blade.php
@@ -5,37 +5,33 @@
 
         @foreach($data as $key =>$datas)
 
-        <a class="product-render rd">
-            <div class="product-slide">
-                <div class="product">
-                    <div class="product-header" href="https://www.nguyenkim.com/dien-thoai-iphone-15-pro-128gb-blue-titanium.html">
-                        <div class="top-right">
-                            <div class="product-feature-badge-item installment"><span>Trả góp 0%</span></div>
-                        </div>
-                        <div class="product-image">
-                            <a href="{{ route('details', $datas->Link) }}">
-                                <img
-                                    class="ls-is-cached lazyloaded"
-                                    
-                                    src="{{ asset($datas->Image) }}"
-                                    
-                                />
-                            </a>
-                        </div>
-                        
-                    </div>
-                    <div class="product-body">
-                        <div class="product-feature-badge"></div>
-                        <div class="product-title"><a href="{{ route('details', $datas->Link) }}">{{ $datas->Name }}</a></div>
-                        <div class="product-price">
-                            <p class="final-price">{{ @str_replace(',' ,'.', number_format($datas->Price)) }}đ   </p>
+        <div class="product col-md-3 col-xs-6">
+            <div class="product-header" href="{{ route('details', $datas->Link) }}">
+                <div class="top-right">
+                    <div class="product-feature-badge-item installment"><span>Trả góp 0%</span></div>
+                </div>
+                <div class="product-image">
+                    <a href="{{ route('details', $datas->Link) }}">
+                        <img
+                            class="ls-is-cached lazyloaded"
                             
-                        </div>
-                    </div>
-                    <div class="product-footer"></div>
+                            src="{{ asset($datas->Image) }}"
+                            
+                        />
+                    </a>
+                </div>
+                
+            </div>
+            <div class="product-body">
+                <div class="product-feature-badge"></div>
+                <div class="product-title"><a href="{{ route('details', $datas->Link) }}">{{ $datas->Name }}</a></div>
+                <div class="product-price">
+                    <p class="final-price">{{ @str_replace(',' ,'.', number_format($datas->Price)) }}đ   </p>
+                    
                 </div>
             </div>
-        </a>
+            <div class="product-footer"></div>
+        </div>
 
         @endforeach
 
